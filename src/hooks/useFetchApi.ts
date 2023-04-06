c07010bf282df7bc43ed3ead8e3d5cc8e84d74c5
SinYongJu <ygp007@naver.com>
2023-04-07 03:17:37 +0900

    FIX: useFetchApi 버그 수정  마운트시 두번  call 하는 내용

--- src/hooks/useFetchApi.ts
+++ src/hooks/useFetchApi.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface UseApiCallProps<ResponseData> {
   initailData: ResponseData;
@@ -11,30 +11,43 @@ export default function useFetchApi<T>({
   fetchFn,
   hasFatchingInMount = true,
 }: UseApiCallProps<T>) {
-  const [isLoading, setLoading] = useState(hasFatchingInMount);
+  const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState<T>(initailData);
   const [error, setError] = useState<unknown>(null);
+  const isFetchingRef = useRef(false);
+  const controllerRef = useRef(new AbortController());
+
+  const fetchData = useCallback(async () => {
+    try {
+      if (isFetchingRef.current) return;
+      isFetchingRef.current = true;
+      setLoading(true);
+      const responseData = await fetchFn(controllerRef.current.signal);
+      setData(responseData);
+    } catch (error) {
+      console.log("error");
+      setError(error);
+    } finally {
+      setLoading(false);
+      isFetchingRef.current = false;
+    }
+  }, [fetchFn]);
+
   useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-    const fetchData = async () => {
-      try {
-        const responseData = await fetchFn(signal);
-        setData(responseData);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    if (isLoading) fetchData();
+    const controller = controllerRef.current;
+    if (hasFatchingInMount) {
+      fetchData();
+    }
     return () => {
+      isFetchingRef.current = false;
       controller.abort();
     };
-  }, [isLoading, fetchFn]);
+  }, [hasFatchingInMount, fetchData]);
 
   const refetch = useCallback(() => {
-    setLoading(true);
-  }, []);
+    controllerRef.current.abort();
+    fetchData();
+  }, [fetchData]);
+
   return { isLoading, error, data, refetch };
 }
