b5e79f8bd126b7ed1187c1abfe38dfcc3e2acfec
SinYongJu <ygp007@naver.com>
2023-04-07 16:37:46 +0900

    UPDATE: Parser 관련된 내용 api로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,9 @@
-import { Order } from "./Models";
-import { delay } from "./utils";
-import "./App.css";
 import { fetchMyOrder } from "./api/v1/order";
 import useFetchApi from "./hooks/useFetchApi";
 import OrderInfo from "./components/order/OrderInfo";
 import ShippingInfo from "./components/order/ShippingInfo";
 import ProductItem from "./components/order/ProductItem";
-
-/**
- * 1. api fetcher 분리
- *  - fetchMyOrder callback > callback (callback hell 가능성 있는 구간에 대하여) Promise 로 처리 하기
- *  - useFetchApi custom hook 생성
- *
- * 2. parse 함수
- * 3. component 분리
- *
- */
-
-const parseOrder = async (json: Record<string, any>): Promise<Order> => {
-  // parse order
-  // response data 를 parse 합니다
-  // 향후 json 변경시, 이 부분에서 변경한다.
-  await delay(500);
-  const shippings = [json.shipping].map((ship) => {
-    ship.products = json.products;
-    return ship;
-  });
-  const order: Order = {
-    id: json.id,
-    orderAt: new Date(json.orderAt),
-    amount: json.amount,
-    shippings,
-  };
-  return order;
-};
-
-const fetchFn = async () => {
-  const json = await fetchMyOrder();
-  const order = await parseOrder(json);
-  return order;
-};
+import "./App.css";
 
 function App() {
   const { isLoading, data, refetch } = useFetchApi({
@@ -59,7 +23,7 @@ function App() {
         },
       ],
     },
-    fetchFn: fetchFn,
+    fetchFn: fetchMyOrder,
   });
   return (
     <div>
--- src/api/v1/order.ts
+++ src/api/v1/order.ts
@@ -1,8 +1,36 @@
+import { Order, OrderResponseData } from "../../Models";
 import assetOrder from "../../assets/order.json";
 import { delay } from "../../utils";
 
-export const fetchMyOrder = async () => {
+export const fetchMyOrder = async (): Promise<Order> => {
   // fetch method
-  await delay(1000);
-  return assetOrder;
+  try {
+    await delay(1000);
+
+    /**
+     *
+     * @param { OrderResponseData } json - order api json format
+     * @returns {Promise<Order>}
+     */
+    const parse = async (json: OrderResponseData): Promise<Order> => {
+      // parse order
+      // response data 를 parse 합니다
+      // 향후 json 변경시, 이 부분에서 변경한다.
+      await delay(500);
+      const shippings = [json.shipping].map((ship) => {
+        return { ...ship, products: json.products };
+      });
+      const order: Order = {
+        id: json.id,
+        orderAt: new Date(json.orderAt),
+        amount: json.amount,
+        shippings,
+      };
+      return order;
+    };
+    const order = await parse(assetOrder);
+    return order;
+  } catch (error) {
+    throw error;
+  }
 };
