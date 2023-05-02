ebfdc1d418b7d7dc19eb0de90d17d03795ddd71a
khil94 <zidru94@gmail.com>
2023-05-02 17:29:05 +0900

    [feat] : 컴포넌트적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { IOrder, IOrderProduct } from "./Models";
+import { IOrder } from "./Models";
 import assetOrder from "./assets/order.json";
+import Order from "./components/Order";
 
 function App() {
   const [isLoading, setLoading] = useState(false);
-  const [orderInfo, setOrderInfo] = useState(Array.of(""));
+  const [order, setOrder] = useState<IOrder>();
+
   useEffect(() => {
     showProgress();
     fetchMyOrder((json) => {
       parseOrder(json, (order) => {
+        setOrder(order);
         hideProgress();
-        presentOrder(order);
       });
     });
   }, []);
 
   const showProgress = () => setLoading(true);
   const hideProgress = () => setLoading(false);
-  const updateOrderInfo = (info: string[]) => setOrderInfo(info);
 
   const fetchMyOrder = (onCompleted: (json: object) => void) => {
     setTimeout(() => {
@@ -32,48 +33,15 @@ function App() {
         id: json.id,
         orderAt: new Date(json.orderAt),
         amount: json.amount,
-        // products: json.products,
         shipping: json.shipping,
       };
       onCompleted(order);
     }, 500);
   };
 
-  const presentOrder = (order: IOrder) => {
-    let output: string[] = [];
-    const println = (text: string) => output.push(text);
-
-    println(`주문번: ${order.id}`);
-    println(`주문일: ${order.orderAt}`);
-    println(`총 결제금액: ${order.amount}원`);
-    println(``);
-    println(`----------------------------`);
-    println(`[상품목록]`);
-    println(``);
-    // order.products.forEach((p: IOrderProduct) => {
-    //   println(`상품명: ${p.name}`);
-    //   println(`가격: ${p.price}원`);
-    //   println(
-    //     `주문정보: ${p.stock.color}/${p.stock.band}/${p.stock.cup} ${p.stock.quantity}개`
-    //   );
-    //   println(``);
-    // });
-    println(`----------------------------`);
-    println(`[배송정보]`);
-    println(`송장번호: ${order.shipping.trackingNumber}`);
-    println(`배송료: ${order.shipping.shippingFee}원`);
-    println(`주소: [${order.shipping.post}] ${order.shipping.address}`);
-    println(`메시지: ${order.shipping.message}`);
-
-    updateOrderInfo(output);
-  };
-
   return (
     <div>
-      {isLoading && <div>Loading...</div>}
-      {orderInfo.map((line) => (
-        <div>{line}</div>
-      ))}
+      {isLoading || !order ? <div>Loading...</div> : <Order {...order} />}
     </div>
   );
 }
