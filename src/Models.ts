cdbfc9ac20f1876b05580d6376718cd142c39536
estemilia <estemilia@gmail.com>
2023-06-12 17:26:15 +0900

    환경설정 및 Model 구조, 예상 json을 변경합니다 - 기존: Order > Shipping + OrderProduct(n) - 변경: Order > Shipping(n) > OrderProduct(n)

--- src/Models.ts
+++ src/Models.ts
@@ -2,30 +2,30 @@ export interface Order {
   id: number;
   orderAt: Date;
   amount: number;
-  products: OrderProduct[];
-  shipping: Shipping;
+  shippings: Shipping[];
 }
 
 export interface OrderProduct {
-  id: number
-  name: string
-  price: number
-  imageUrls: string[]
-  stock: Stock
+  id: number;
+  name: string;
+  price: number;
+  imageUrls: string[];
+  stock: Stock;
 }
 
 export interface Stock {
-  color: string
-  band: string
-  cup: string
-  quantity: number
+  color: string;
+  band: string;
+  cup: string;
+  quantity: number;
 }
 
 export interface Shipping {
-  id: number,
-  trackingNumber: string,
-  shippingFee: number,
-  address: string,
-  post: string,
-  message: string
+  id: number;
+  trackingNumber: string;
+  shippingFee: number;
+  address: string;
+  post: string;
+  message: string;
+  products: OrderProduct[];
 }
