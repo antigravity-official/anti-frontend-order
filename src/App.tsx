3ff61482ffce9243473c62d6c8da78c15e6630b4
JHscar <wogns5206@naver.com>
2023-07-12 01:46:44 +0900

    1. App.test.tsx -> getByText 문구 변경 2. useEffect 내부 코드 loadOrder로 분리 3. async/await를 사용하여 비동기 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-import {Order, OrderProduct} from "./Models";
-import assetOrder from './assets/order.json'
+import { Order } from './Models';
+import assetOrder from './assets/order.json';
+
+// utils
+import { fetchMyOrder, parseOrder } from './utils/utils';
+
+// components
+import OrderInfo from './components/OrderInfo';
 
 function App() {
     const [isLoading, setLoading] = useState(false);
-    const [orderInfo, setOrderInfo] = useState(Array.of(""));
+    const [order, setOrder] = useState<Order | null>(null);
+
     useEffect(() => {
-        showProgress();
-        fetchMyOrder(json => {
-            parseOrder(json, order => {
-                hideProgress();
-                presentOrder(order)
-            });
-        });
+        loadOrder();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+    
+    const loadOrder = async () => {
+        showProgress();
+        const json = await fetchMyOrder(assetOrder);
+        const order = await parseOrder(json);
+        hideProgress();
+        setOrder(order);
+    };
 
     const showProgress = () => setLoading(true);
     const hideProgress = () => setLoading(false);
-    const updateOrderInfo = (info: string[]) => setOrderInfo(info);
-
-
-    const fetchMyOrder = (onCompleted: (json: object) => void) => {
-        setTimeout(() => {
-            onCompleted(assetOrder)
-        }, 1000)
-    }
-
-    const parseOrder = (json: any, onCompleted: (order: Order) => void) => {
-        setTimeout(() => {
-            const order: Order = {
-                id: json.id,
-                orderAt: new Date(json.orderAt),
-                amount: json.amount,
-                products: json.products,
-                shipping: json.shipping,
-            }
-            onCompleted(order)
-        }, 500)
-    }
-
-    const presentOrder = (order: Order) => {
-        let output: string[] = [];
-        const println = (text: string) => output.push(text);
-
-        println(`주문번: ${order.id}`)
-        println(`주문일: ${order.orderAt}`)
-        println(`총 결제금액: ${order.amount}원`)
-        println(``)
-        println(`----------------------------`)
-        println(`[상품목록]`)
-        println(``)
-        order.products.forEach((p: OrderProduct) => {
-            println(`상품명: ${p.name}`)
-            println(`가격: ${p.price}원`)
-            println(`주문정보: ${p.stock.color}/${p.stock.band}/${p.stock.cup} ${p.stock.quantity}개`)
-            println(``)
-        })
-        println(`----------------------------`)
-        println(`[배송정보]`)
-        println(`송장번호: ${order.shipping.trackingNumber}`)
-        println(`배송료: ${order.shipping.shippingFee}원`)
-        println(`주소: [${order.shipping.post}] ${order.shipping.address}`)
-        println(`메시지: ${order.shipping.message}`)
-
-        updateOrderInfo(output);
-    }
 
     return (
         <div>
             {isLoading && <div>Loading...</div>}
-            {orderInfo.map(line => <div>{line}</div>)}
+            {order && <OrderInfo order={order} />}
         </div>
     );
 }
