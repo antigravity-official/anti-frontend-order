99ae9f407a2a8f73c5be5b1c447fbf3537774ed2
Yena-Yun <cheryleduart@gmail.com>
2023-07-15 00:11:14 +0900

    Refactor: Model.ts 변경

--- src/model/Models.ts
+++ src/model/Models.ts
@@ -2,30 +2,30 @@ export interface Order {
   id: number;
   orderAt: Date;
   amount: number;
-  products: OrderProduct[];
-  shipping: Shipping;
-}
-
-export interface OrderProduct {
-  id: number
-  name: string
-  price: number
-  imageUrls: string[]
-  stock: Stock
-}
-
-export interface Stock {
-  color: string
-  band: string
-  cup: string
-  quantity: number
+  shipping: Shipping[];
 }
 
 export interface Shipping {
-  id: number,
-  trackingNumber: string,
-  shippingFee: number,
-  address: string,
-  post: string,
-  message: string
+  id: number;
+  trackingNumber: string;
+  shippingFee: number;
+  address: string;
+  post: string;
+  message: string;
+  products: OrderProduct[];
+}
+
+export interface OrderProduct {
+  id: number;
+  name: string;
+  price: number;
+  imageUrls: string[];
+  stock: Stock;
+}
+
+export interface Stock {
+  color: string;
+  band: string;
+  cup: string;
+  quantity: number;
 }
