1391fbff761d6d4ac22acddc32018c20231e9d9b
sumnii <song.sumin.kr@gmail.com>
2023-07-16 21:18:59 +0900

    refact: Divider 컴포넌트 분리

--- src/components/orderInfo/OrderInfo.tsx
+++ src/components/orderInfo/OrderInfo.tsx
@@ -1,4 +1,5 @@
 import * as T from "../../type/Models";
+import Divider from "./Divider";
 import ProductInfo from "./ProductInfo";
 import ShippingInfo from "./ShippingInfo";
 
@@ -13,15 +14,15 @@ export default function OrderInfo({ data }: OrderInfoProps) {
   println(`주문번호: ${data?.id}`);
   println(`주문일: ${data?.orderAt}`);
   println(`총 결제금액: ${data?.amount}원`);
-  println(``);
-  println(`----------------------------`);
 
   return (
     <div>
       {output.map((line) => (
         <div>{line}</div>
       ))}
+      <Divider />
       {data?.products && <ProductInfo data={data.products} />}
+      <Divider />
       {data?.shipping && <ShippingInfo data={data.shipping} />}
     </div>
   );
